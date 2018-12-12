927fc8e513ade09d91b9c0e90f5315f978ec4f12
VAMDeluxe <vamdeluxe@gmail.com>
2018-12-12 15:13:46 -0800

    added an always in edit mode script, added select root

---
+++ Powertools/AlwaysEdit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+
+namespace DeluxePlugin.Powertools
+{
+    public class AlwaysEdit : MVRScript
+    {
+        void Update()
+        {
+            try
+            {
+                if (SuperController.singleton.editModeToggle.isOn == false)
+                {
+                    SuperController.singleton.editModeToggle.isOn = true;
+                }
+            }
+            catch (Exception e)
+            {
+                SuperController.LogError("Exception caught: " + e);
+            }
+        }
+
+
+
+    }
+}
---
+++ Powertools/SelectRoot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+
+namespace DeluxePlugin.Powertools
+{
+    public class SelectRoot : MVRScript
+    {
+        FreeControllerV3 lastSelected;
+        float lastPressedTime = 0;
+        float doubleClickTime = 0.5f;
+        void LateUpdate()
+        {
+            try
+            {
+                if (Input.GetMouseButtonUp(0))
+                {
+                    if ((Time.time - lastPressedTime) < doubleClickTime && SuperController.singleton.GetSelectedController() == lastSelected)
+                    {
+                        SelectMainController();
+                    }
+
+                    lastSelected = SuperController.singleton.GetSelectedController();
+                    lastPressedTime = Time.time;
+                }
+
+                if (Input.GetMouseButtonUp(0) && (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)))
+                {
+                    SelectMainController();
+                }
+            }
+            catch (Exception e)
+            {
+                SuperController.LogError("Exception caught: " + e);
+            }
+        }
+
+        void SelectMainController()
+        {
+            FreeControllerV3 selected = SuperController.singleton.GetSelectedController();
+
+            if (selected == null)
+            {
+                return;
+            }
+
+            SuperController.singleton.SelectController(selected.containingAtom.mainController);
+            SuperController.singleton.ShowMainHUD();
+
+        }
+
+    }
+}
